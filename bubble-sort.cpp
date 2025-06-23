81a833801d776827243ad020ac673e13c16d8219
Faysal Ahmed Fahim <153068178+0xFaysal@users.noreply.github.com>
2025-06-23 17:39:14 +0600

    initial git

---
+++ bubble-sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[], int n);
+
+int main()
+{
+
+    return 0;
+}
+
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+
+        }
+    }
+}
