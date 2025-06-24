753f8e1bd9c6831800ea7e557ba30615400c58e8
Faysal Ahmed Fahim <153068178+0xFaysal@users.noreply.github.com>
2025-06-24 11:27:43 +0600

    add graph and sorting algorithms: implement BFS, DFS, BST, and sorting functions

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -5,6 +5,19 @@ void bubbleSort(int arr[], int n);
 
 int main()
 {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted array: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    bubbleSort(arr, n);
+    cout << "\nSorted array: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+
 
     return 0;
 }
--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -1,8 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void insertionSort(int arr[], int n);
+
+
 int main()
 {
-    cout<<"Hello World"<<endl;
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Unsorted array: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    insertionSort(arr, n);
+    cout << "\nSorted array: \n";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
     return 0;
+}
+
+
+void insertionSort(int arr[], int n)
+{
+    for(int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while(j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
 }
